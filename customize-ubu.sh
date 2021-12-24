0f0cfc842a14a837532921a171b989dd738e8368
nabucco <nabucco@example.com>
2021-12-24 18:43:21 +0100

    sudo

--- customize-ubu.sh
+++ customize-ubu.sh
@@ -5,7 +5,7 @@ CU_HOSTNAME9=ubu-1.local
 
 HOSTS=/etc/hosts
 cp -v $HOSTS $HOSTS.bak
-awk -i inplace \
+sudo awk -i inplace \
   'tolower($2)=="'$HOSTNAME'"{sub($2,"'$CU_HOSTNAME9'",$2)}; \
   {print}' \
   $HOSTS
