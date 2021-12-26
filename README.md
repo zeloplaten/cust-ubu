8d661c36db1cf120579e58b4bdba45c6a60d1ed1
nabucco <nabucco@example.com>
2021-12-26 17:07:07 +0100

    chg console font

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 #### Customize ubuntu server installation
 
-#### download customizer script
+download customizer script
 $ CFILE=customize-ubu.sh \
 $ wget https://raw.githubusercontent.com/zeloplaten/cust-ubu/main/$CFILE -O $CFILE
 
-#### edit script variables as desired
+edit script variables as desired
 $ nano $CFILE
 
-#### execute script as root
-$ sudo su root \
-$ chmod 744 $CFILE \
+execute script as root
+$ sudo su \
+$ chmod +x $CFILE \
 $ ./$CFILE
--- customize-ubu.sh
+++ customize-ubu.sh
@@ -1,9 +1,30 @@
 # edit variables
 # to choose new hostname
-CU_HOSTNAME9=ubu-1.local
+CU_HOSTNAME9=ubu-1
 #
 
-HOSTS=/etc/hosts
-cp -v $HOSTS $HOSTS.bak
-awk -i inplace 'tolower($2)=="'$HOSTNAME'"{sub($2,"'$CU_HOSTNAME9'",$2)}; {print}' $HOSTS
+# change hostname
+
+HN0=$(hostname)  # don't rely on $HOSTNAME
 hostnamectl set-hostname $CU_HOSTNAME9
+HN9=$(hostname)  # use effective (new) hostname
+HOSTS=/etc/hosts
+sed --in-place=.bak \
+  --expression='s/^/ /; s/$/ /;# insert leading and trailing blank' \
+  --expression=':1;s/ '$HN0' / '$HN9' /ig;t1;# repeated substitution' \
+  --expression='s/^ //; s/ $//;# remove inserted blanks' \
+  $HOSTS
+
+# change console font
+
+CFILE=/etc/default/console-setup
+touch $CFILE
+sed --in-place=.bak \
+  --expression='/^FONTFACE=/d' \
+  --expression='/^FONTSIZE=/d' \
+  $CFILE
+echo '' >> $CFILE
+echo 'FONTFACE="Terminus"' >> $CFILE
+echo 'FONTSIZE="16x32"' >> $CFILE
+
+update-initramfs -u
